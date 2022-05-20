e567f6d53708d9a9fd532f4e8fbe0f5fbc54286c
Matias-sh <devmatiasbritez@gmail.com>
2022-05-20 18:00:05 -0300

    borrado archivos staticos de más y finalizado la sección servicios

--- inicio/views.py
+++ inicio/views.py
@@ -15,5 +15,49 @@ def obras(request):
 def servicios(request):
     return render(request, 'inicio/servicios.html')
 
+## VISTAS SERVICIOS
+
 def proyecto(request):
-    return render(request, 'inicio/servicios/proyecto.html')
+    return render(request, 'inicio/servicios/proyecto/servicio.html')
+
+def administracion(request):
+    return render(request, 'inicio/servicios/administracion/servicio.html')
+
+def carpinteria(request):
+    return render(request, 'inicio/servicios/carpinteria/servicio.html')
+
+def disenio(request):
+    return render(request, 'inicio/servicios/disenio/servicio.html')
+
+def disenios_interiores(request):
+    return render(request, 'inicio/servicios/disenios_interiores/servicio.html')
+
+def drywall(request):
+    return render(request, 'inicio/servicios/drywall/servicio.html')
+
+def ejecucion_obras(request):
+    return render(request, 'inicio/servicios/ejecucion_obras/servicio.html')
+
+def estrucymon_galpones(request):
+    return render(request, 'inicio/servicios/estrucymon_galpones/servicio.html')
+
+def estrucymon_techos(request):
+    return render(request, 'inicio/servicios/estrucymon_techos/servicio.html')
+
+def pintura(request):
+    return render(request, 'inicio/servicios/pintura/servicio.html')
+
+def plomeria(request):
+    return render(request, 'inicio/servicios/plomeria/servicio.html')
+
+def remodelaciones(request):
+    return render(request, 'inicio/servicios/remodelaciones/servicio.html')
+
+def restauraciones(request):
+    return render(request, 'inicio/servicios/restauraciones/servicio.html')
+
+def sist_electricos(request):
+    return render(request, 'inicio/servicios/sist_electricos/servicio.html')
+
+def steel_framing(request):
+    return render(request, 'inicio/servicios/steel_framing/servicio.html')
