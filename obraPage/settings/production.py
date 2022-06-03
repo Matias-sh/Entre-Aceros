687cdf7bd550b2896d244333483986015e393286
Matias-sh <devmatiasbritez@gmail.com>
2022-06-03 12:28:00 -0300

    prueba para contacto

--- obraPage/settings/production.py
+++ obraPage/settings/production.py
@@ -3,7 +3,8 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['secret-forest-35723.herokuapp.com', '*']
+ALLOWED_HOSTS = ['secret-forest-35723.herokuapp.com', '*','entreaceros.com.ar','entre-aceros.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://entreaceros.com.ar']
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
